8d0eb03e0d79fe4385fff4e0548d408336bb587d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-02-04 09:49:47 -0700

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,10 +4,8 @@
 #include <fstream>
 #include <android/log.h>
 #include <android/bitmap.h>
-#include <aaudio/AAudio.h>
 #include <thread>
 #include <chrono>
-#include <math.h>
 
 #define LOG_TAG "DWA_NATIVE"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
@@ -15,9 +13,8 @@
 // --- NES Hardware State ---
 uint8_t cpu_ram[0x0800];
 uint8_t prg_rom[4][16384];             
-uint8_t chr_rom[8192];                 
+uint8_t chr_rom[8192];  // 8KB of graphics data               
 uint8_t sram[0x2000];                  
-std::string global_save_path;
 
 // --- Video State ---
 uint32_t screen_buffer[256 * 240];
@@ -51,7 +48,6 @@ bool is_running = false;
 uint8_t current_prg_bank = 0;
 uint8_t mmc1_shift_reg = 0x10;
 
-// Memory Helpers
 uint8_t read_byte(uint16_t addr) {
     if (addr < 0x2000) return cpu_ram[addr % 0x0800];
     if (addr >= 0x6000 && addr < 0x8000) return sram[addr - 0x6000];
@@ -76,7 +72,6 @@ void write_byte(uint16_t addr, uint8_t val) {
     }
 }
 
-// 6502 CPU Implementation (Full Instruction Set)
 int step_cpu() {
     uint8_t op = read_byte(reg_PC++);
     uint16_t addr = 0;
@@ -84,115 +79,66 @@ int step_cpu() {
 
     switch (op) {
         // --- DATA TRANSFER ---
-        case 0xA9: reg_A = read_byte(reg_PC++); SET_ZN(reg_A); return 2; // LDA Imm
-        case 0xA5: reg_A = read_byte(read_byte(reg_PC++)); SET_ZN(reg_A); return 3; // LDA ZP
-        case 0xAD: { uint16_t l = read_byte(reg_PC++), h = read_byte(reg_PC++); reg_A = read_byte((h<<8)|l); SET_ZN(reg_A); return 4; } // LDA Abs
-        case 0xA2: reg_X = read_byte(reg_PC++); SET_ZN(reg_X); return 2; // LDX Imm
-        case 0xA6: reg_X = read_byte(read_byte(reg_PC++)); SET_ZN(reg_X); return 3; // LDX ZP
-        case 0xA0: reg_Y = read_byte(reg_PC++); SET_ZN(reg_Y); return 2; // LDY Imm
-        case 0x85: write_byte(read_byte(reg_PC++), reg_A); return 3; // STA ZP
-        case 0x8D: { uint16_t l = read_byte(reg_PC++), h = read_byte(reg_PC++); write_byte((h<<8)|l, reg_A); return 4; } // STA Abs
-        case 0x86: write_byte(read_byte(reg_PC++), reg_X); return 3; // STX ZP
-        case 0x84: write_byte(read_byte(reg_PC++), reg_Y); return 3; // STY ZP
-        case 0xAA: reg_X = reg_A; SET_ZN(reg_X); return 2; // TAX
-        case 0xA8: reg_Y = reg_A; SET_ZN(reg_Y); return 2; // TAY
-        case 0x8A: reg_A = reg_X; SET_ZN(reg_A); return 2; // TXA
-        case 0x98: reg_A = reg_Y; SET_ZN(reg_A); return 2; // TYA
+        case 0xA9: reg_A = read_byte(reg_PC++); SET_ZN(reg_A); return 2;
+        case 0xA5: reg_A = read_byte(read_byte(reg_PC++)); SET_ZN(reg_A); return 3;
+        case 0xAD: { uint16_t l = read_byte(reg_PC++), h = read_byte(reg_PC++); reg_A = read_byte((h<<8)|l); SET_ZN(reg_A); return 4; }
+        case 0xA2: reg_X = read_byte(reg_PC++); SET_ZN(reg_X); return 2;
+        case 0xA6: reg_X = read_byte(read_byte(reg_PC++)); SET_ZN(reg_X); return 3;
+        case 0xA0: reg_Y = read_byte(reg_PC++); SET_ZN(reg_Y); return 2;
+        case 0x85: write_byte(read_byte(reg_PC++), reg_A); return 3;
+        case 0x8D: { uint16_t l = read_byte(reg_PC++), h = read_byte(reg_PC++); write_byte((h<<8)|l, reg_A); return 4; }
+        case 0x86: write_byte(read_byte(reg_PC++), reg_X); return 3;
+        case 0x84: write_byte(read_byte(reg_PC++), reg_Y); return 3;
+        case 0xAA: reg_X = reg_A; SET_ZN(reg_X); return 2;
+        case 0xA8: reg_Y = reg_A; SET_ZN(reg_Y); return 2;
+        case 0x8A: reg_A = reg_X; SET_ZN(reg_A); return 2;
+        case 0x98: reg_A = reg_Y; SET_ZN(reg_A); return 2;
 
-        // --- STACK OPERATIONS ---
-        case 0x9A: reg_S = reg_X; return 2; // TXS
-        case 0xBA: reg_X = reg_S; SET_ZN(reg_X); return 2; // TSX
-        case 0x48: write_byte(0x0100 + reg_S--, reg_A); return 3; // PHA
-        case 0x68: reg_A = read_byte(0x0100 + ++reg_S); SET_ZN(reg_A); return 4; // PLA
-        case 0x08: write_byte(0x0100 + reg_S--, reg_P | 0x30); return 3; // PHP
-        case 0x28: reg_P = read_byte(0x0100 + ++reg_S) | FLAG_U; return 4; // PLP
+        // --- STACK ---
+        case 0x9A: reg_S = reg_X; return 2;
+        case 0xBA: reg_X = reg_S; SET_ZN(reg_X); return 2;
+        case 0x48: write_byte(0x0100 + reg_S--, reg_A); return 3;
+        case 0x68: reg_A = read_byte(0x0100 + ++reg_S); SET_ZN(reg_A); return 4;
+        case 0x08: write_byte(0x0100 + reg_S--, reg_P | 0x30); return 3;
+        case 0x28: reg_P = read_byte(0x0100 + ++reg_S) | FLAG_U; return 4;
 
-        // --- ARITHMETIC & LOGIC ---
-        case 0x69: { // ADC Imm
-            uint8_t val = read_byte(reg_PC++);
-            uint16_t res = (uint16_t)reg_A + (uint16_t)val + (reg_P & FLAG_C);
-            if (res > 0xFF) reg_P |= FLAG_C; else reg_P &= ~FLAG_C;
-            if (!((reg_A ^ val) & 0x80) && ((reg_A ^ res) & 0x80)) reg_P |= FLAG_V; else reg_P &= ~FLAG_V;
-            reg_A = (uint8_t)res; SET_ZN(reg_A); return 2;
-        }
-        case 0xE9: { // SBC Imm
-            uint8_t val = read_byte(reg_PC++);
-            uint16_t res = (uint16_t)reg_A - (uint16_t)val - ((reg_P & FLAG_C) ? 0 : 1);
-            if (!(res & 0xFF00)) reg_P |= FLAG_C; else reg_P &= ~FLAG_C;
-            if (((reg_A ^ val) & 0x80) && ((reg_A ^ res) & 0x80)) reg_P |= FLAG_V; else reg_P &= ~FLAG_V;
-            reg_A = (uint8_t)res; SET_ZN(reg_A); return 2;
-        }
-        case 0xC9: temp = read_byte(reg_PC++); if(reg_A >= temp) reg_P |= FLAG_C; else reg_P &= ~FLAG_C; SET_ZN(reg_A - temp); return 2; // CMP Imm
-        case 0xE0: temp = read_byte(reg_PC++); if(reg_X >= temp) reg_P |= FLAG_C; else reg_P &= ~FLAG_C; SET_ZN(reg_X - temp); return 2; // CPX Imm
-        case 0xC0: temp = read_byte(reg_PC++); if(reg_Y >= temp) reg_P |= FLAG_C; else reg_P &= ~FLAG_C; SET_ZN(reg_Y - temp); return 2; // CPY Imm
-        case 0x29: reg_A &= read_byte(reg_PC++); SET_ZN(reg_A); return 2; // AND Imm
-        case 0x09: reg_A |= read_byte(reg_PC++); SET_ZN(reg_A); return 2; // ORA Imm
-        case 0x49: reg_A ^= read_byte(reg_PC++); SET_ZN(reg_A); return 2; // EOR Imm
+        // --- MATH ---
+        case 0x69: { uint8_t val = read_byte(reg_PC++); uint16_t res = (uint16_t)reg_A + (uint16_t)val + (reg_P & FLAG_C); if (res > 0xFF) reg_P |= FLAG_C; else reg_P &= ~FLAG_C; if (!((reg_A ^ val) & 0x80) && ((reg_A ^ res) & 0x80)) reg_P |= FLAG_V; else reg_P &= ~FLAG_V; reg_A = (uint8_t)res; SET_ZN(reg_A); return 2; }
+        case 0xE9: { uint8_t val = read_byte(reg_PC++); uint16_t res = (uint16_t)reg_A - (uint16_t)val - ((reg_P & FLAG_C) ? 0 : 1); if (!(res & 0xFF00)) reg_P |= FLAG_C; else reg_P &= ~FLAG_C; if (((reg_A ^ val) & 0x80) && ((reg_A ^ res) & 0x80)) reg_P |= FLAG_V; else reg_P &= ~FLAG_V; reg_A = (uint8_t)res; SET_ZN(reg_A); return 2; }
+        case 0xC9: temp = read_byte(reg_PC++); if(reg_A >= temp) reg_P |= FLAG_C; else reg_P &= ~FLAG_C; SET_ZN(reg_A - temp); return 2;
+        case 0x29: reg_A &= read_byte(reg_PC++); SET_ZN(reg_A); return 2;
+        case 0x09: reg_A |= read_byte(reg_PC++); SET_ZN(reg_A); return 2;
+        case 0x49: reg_A ^= read_byte(reg_PC++); SET_ZN(reg_A); return 2;
 
-        // --- INCREMENT / DECREMENT ---
-        case 0xE8: reg_X++; SET_ZN(reg_X); return 2; // INX
-        case 0xC8: reg_Y++; SET_ZN(reg_Y); return 2; // INY
-        case 0xCA: reg_X--; SET_ZN(reg_X); return 2; // DEX
-        case 0x88: reg_Y--; SET_ZN(reg_Y); return 2; // DEY
-        case 0xE6: { uint8_t a = read_byte(reg_PC++); temp = read_byte(a) + 1; write_byte(a, temp); SET_ZN(temp); return 5; } // INC ZP
-        case 0xC6: { uint8_t a = read_byte(reg_PC++); temp = read_byte(a) - 1; write_byte(a, temp); SET_ZN(temp); return 5; } // DEC ZP
+        // --- BRANCH ---
+        case 0x4C: { uint16_t l = read_byte(reg_PC++), h = read_byte(reg_PC++); reg_PC = (h<<8)|l; return 3; }
+        case 0x20: { uint16_t l = read_byte(reg_PC++), h = read_byte(reg_PC++); uint16_t ret = reg_PC - 1; write_byte(0x0100 + reg_S--, (ret >> 8) & 0xFF); write_byte(0x0100 + reg_S--, ret & 0xFF); reg_PC = (h << 8) | l; return 6; }
+        case 0x60: { uint16_t l = read_byte(0x0100 + ++reg_S); uint16_t h = read_byte(0x0100 + ++reg_S); reg_PC = ((h << 8) | l) + 1; return 6; }
+        case 0xD0: temp = read_byte(reg_PC++); if(!(reg_P & FLAG_Z)) reg_PC += (int8_t)temp; return 2;
+        case 0xF0: temp = read_byte(reg_PC++); if(reg_P & FLAG_Z) reg_PC += (int8_t)temp; return 2;
 
-        // --- CONTROL FLOW ---
-        case 0x4C: { uint16_t l = read_byte(reg_PC++), h = read_byte(reg_PC++); reg_PC = (h<<8)|l; return 3; } // JMP Abs
-        case 0x20: { // JSR
-            uint16_t l = read_byte(reg_PC++), h = read_byte(reg_PC++);
-            uint16_t ret = reg_PC - 1;
-            write_byte(0x0100 + reg_S--, (ret >> 8) & 0xFF);
-            write_byte(0x0100 + reg_S--, ret & 0xFF);
-            reg_PC = (h << 8) | l; return 6;
-        }
-        case 0x60: { // RTS
-            uint16_t l = read_byte(0x0100 + ++reg_S);
-            uint16_t h = read_byte(0x0100 + ++reg_S);
-            reg_PC = ((h << 8) | l) + 1; return 6;
-        }
-        case 0x40: { // RTI
-            reg_P = read_byte(0x0100 + ++reg_S) | FLAG_U;
-            uint16_t l = read_byte(0x0100 + ++reg_S);
-            uint16_t h = read_byte(0x0100 + ++reg_S);
-            reg_PC = (h << 8) | l; return 6;
-        }
-
-        // --- BRANCHES ---
-        case 0x90: temp = read_byte(reg_PC++); if(!(reg_P & FLAG_C)) reg_PC += (int8_t)temp; return 2; // BCC
-        case 0xB0: temp = read_byte(reg_PC++); if(reg_P & FLAG_C) reg_PC += (int8_t)temp; return 2; // BCS
-        case 0xF0: temp = read_byte(reg_PC++); if(reg_P & FLAG_Z) reg_PC += (int8_t)temp; return 2; // BEQ
-        case 0xD0: temp = read_byte(reg_PC++); if(!(reg_P & FLAG_Z)) reg_PC += (int8_t)temp; return 2; // BNE
-        case 0x10: temp = read_byte(reg_PC++); if(!(reg_P & FLAG_N)) reg_PC += (int8_t)temp; return 2; // BPL
-        case 0x30: temp = read_byte(reg_PC++); if(reg_P & FLAG_N) reg_PC += (int8_t)temp; return 2; // BMI
-
-        // --- FLAG MODIFICATION ---
-        case 0x18: reg_P &= ~FLAG_C; return 2; // CLC
-        case 0x38: reg_P |= FLAG_C; return 2; // SEC
-        case 0x58: reg_P &= ~FLAG_I; return 2; // CLI
-        case 0x78: reg_P |= FLAG_I; return 2; // SEI
-        case 0xD8: reg_P &= ~FLAG_D; return 2; // CLD
-        case 0xF8: reg_P |= FLAG_D; return 2; // SED
-
-        // --- MISC ---
-        case 0xEA: return 2; // NOP
-        case 0x00: return 7; // BRK (Stub)
-
-        default: 
-            return 1;
+        // --- FLAGS ---
+        case 0x18: reg_P &= ~FLAG_C; return 2;
+        case 0x38: reg_P |= FLAG_C; return 2;
+        case 0x78: reg_P |= FLAG_I; return 2;
+        case 0xEA: return 2;
+        default: return 1;
     }
 }
 
-// Render Logic
+// Updated rendering logic for Dragon Warrior
 void render_frame() {
+    // Basic Background Rendering (Name Table at 0x2000 in NES PPU map, roughly translated here)
     for (int t = 0; t < 960; t++) {
-        int tile_id = cpu_ram[0x0400 + t];
+        int tile_id = cpu_ram[0x0400 + t]; // Dummy mapping to CPU RAM for background
         int xb = (t % 32) * 8, yb = (t / 32) * 8;
         for (int row = 0; row < 8; row++) {
-            uint8_t p1 = chr_rom[tile_id * 16 + row], p2 = chr_rom[tile_id * 16 + row + 8];
+            uint8_t p1 = chr_rom[tile_id * 16 + row];
+            uint8_t p2 = chr_rom[tile_id * 16 + row + 8];
             for (int col = 0; col < 8; col++) {
                 int pix = ((p1 >> (7 - col)) & 1) | (((p2 >> (7 - col)) & 1) << 1);
-                screen_buffer[(yb + row) * 256 + (xb + col)] = nes_palette[pix + 0x0F];
+                if (pix != 0) screen_buffer[(yb + row) * 256 + (xb + col)] = nes_palette[pix + 0x01];
+                else screen_buffer[(yb + row) * 256 + (xb + col)] = 0xFF000000; // Black
             }
         }
     }
@@ -213,10 +159,17 @@ extern "C" JNIEXPORT void JNICALL
 Java_com_canc_dwa_MainActivity_nativeInitEngine(JNIEnv* env, jobject thiz, jstring filesDir) {
     const char* cPath = env->GetStringUTFChars(filesDir, nullptr);
     std::string pathStr(cPath);
+    
+    // Load PRG Banks
     for(int i = 0; i < 4; i++) {
         std::ifstream in(pathStr + "/prg_bank_" + std::to_string(i) + ".bin", std::ios::binary);
         if (in.is_open()) in.read((char*)prg_rom[i], 16384);
     }
+    
+    // Load CHR Graphics (CRITICAL FIX)
+    std::ifstream chr_in(pathStr + "/chr_rom.bin", std::ios::binary);
+    if (chr_in.is_open()) chr_in.read((char*)chr_rom, 8192);
+
     reg_PC = (read_byte(0xFFFD) << 8) | read_byte(0xFFFC);
     reg_S = 0xFD; 
     reg_P = FLAG_I | FLAG_U;
@@ -238,13 +191,24 @@ Java_com_canc_dwa_MainActivity_nativeExtractRom(JNIEnv* env, jobject thiz, jstri
     const char* cRomPath = env->GetStringUTFChars(romPath, nullptr);
     const char* cOutDir = env->GetStringUTFChars(outDir, nullptr);
     std::ifstream in(cRomPath, std::ios::binary);
+    
+    // Skip 16-byte iNES Header
     in.seekg(16);
+    
+    // Extract 4 PRG Banks (64KB total)
     for (int i = 0; i < 4; i++) {
         std::ofstream out(std::string(cOutDir) + "/prg_bank_" + std::to_string(i) + ".bin", std::ios::binary);
         char bankData[16384];
         in.read(bankData, 16384);
         out.write(bankData, 16384);
     }
+
+    // Extract CHR ROM (8KB total) (CRITICAL FIX)
+    std::ofstream chr_out(std::string(cOutDir) + "/chr_rom.bin", std::ios::binary);
+    char chrData[8192];
+    in.read(chrData, 8192);
+    chr_out.write(chrData, 8192);
+
     env->ReleaseStringUTFChars(romPath, cRomPath);
     env->ReleaseStringUTFChars(outDir, cOutDir);
     return env->NewStringUTF("Success");
