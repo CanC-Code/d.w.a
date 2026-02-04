9be94e933ce2219dec5a6299dc690cee4544feef
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-02-04 16:32:55 -0700

    Update MapperMMC1.h

--- app/src/main/cpp/MapperMMC1.h
+++ app/src/main/cpp/MapperMMC1.h
@@ -7,7 +7,7 @@
 class MapperMMC1 {
 public:
     uint8_t prg_rom[4][16384]; // 4 banks of 16KB
-    uint8_t chr_rom[2][4096];  // 2 banks of 4KB
+    uint8_t chr_rom[4][4096];  // 4 banks of 4KB (Total 16KB)
     
     uint8_t shift_register = 0x10;
     uint8_t control = 0x0C; 
@@ -46,14 +46,14 @@ public:
         if (addr >= 0x8000 && addr < 0xC000) {
             return prg_rom[prg_bank % 4][addr - 0x8000];
         } else if (addr >= 0xC000) {
-            return prg_rom[3][addr - 0xC000]; // Last bank fixed
+            return prg_rom[3][addr - 0xC000]; // Last bank fixed at $C000
         }
         return 0;
     }
 
     uint8_t read_chr(uint16_t addr) {
-        if (addr < 0x1000) return chr_rom[chr_bank_0 % 2][addr];
-        return chr_rom[chr_bank_1 % 2][addr - 0x1000];
+        if (addr < 0x1000) return chr_rom[chr_bank_0 % 4][addr];
+        return chr_rom[chr_bank_1 % 4][addr - 0x1000];
     }
 };
 
