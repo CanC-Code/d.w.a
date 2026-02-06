9f5ed23a38056d2b17e96643b1b3fc9745c1c86c
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-02-05 19:02:37 -0700

    Update MapperMMC1.h

--- app/src/main/cpp/MapperMMC1.h
+++ app/src/main/cpp/MapperMMC1.h
@@ -6,16 +6,17 @@
 
 class MapperMMC1 {
 public:
-    // Dragon Warrior I: 64KB PRG (4x16KB) and up to 32KB CHR (8x4KB)
+    // Dragon Warrior I: 64KB PRG (4x16KB) and 16KB CHR (4x4KB)
     uint8_t prg_rom[4][16384]; 
     uint8_t chr_rom[8][4096];
-    uint8_t prg_ram[8192]; // Added for Save Game/SRAM support ($6000-$7FFF)
+    uint8_t prg_ram[8192]; 
 
     uint8_t shift_register;
     uint8_t control;
     uint8_t chr_bank_0;
     uint8_t chr_bank_1;
     uint8_t prg_bank;
+    bool ram_enabled;
 
     MapperMMC1() {
         reset();
@@ -28,10 +29,12 @@ public:
         prg_bank = 0;
         chr_bank_0 = 0;
         chr_bank_1 = 0;
+        ram_enabled = true; // Default to true for boot safety
         std::memset(prg_ram, 0, sizeof(prg_ram));
     }
 
     void write(uint16_t addr, uint8_t val) {
+        // Reset shift register if bit 7 is set
         if (val & 0x80) {
             shift_register = 0x10;
             control |= 0x0C;
@@ -42,33 +45,41 @@ public:
 
             if (complete) {
                 uint8_t data = shift_register & 0x1F;
-                if (addr >= 0x8000 && addr <= 0x9FFF) control = data;
-                else if (addr >= 0xA000 && addr <= 0xBFFF) chr_bank_0 = data;
-                else if (addr >= 0xC000 && addr <= 0xDFFF) chr_bank_1 = data;
-                else if (addr >= 0xE000) prg_bank = data & 0x0F;
-                
+                if (addr >= 0x8000 && addr <= 0x9FFF) {
+                    control = data;
+                } else if (addr >= 0xA000 && addr <= 0xBFFF) {
+                    chr_bank_0 = data;
+                } else if (addr >= 0xC000 && addr <= 0xDFFF) {
+                    chr_bank_1 = data;
+                } else if (addr >= 0xE000) {
+                    prg_bank = data & 0x0F;
+                    // Bit 4 of PRG Bank register enables/disables RAM on some MMC1 versions
+                    ram_enabled = !(data & 0x10); 
+                }
                 shift_register = 0x10;
             }
         }
     }
 
     uint8_t read_prg(uint16_t addr) {
-        // Handle PRG RAM (SRAM) access
-        if (addr >= 0x6000 && addr <= 0x7FFF) return prg_ram[addr - 0x6000];
+        // Handle PRG RAM (SRAM) access ($6000-$7FFF)
+        if (addr >= 0x6000 && addr <= 0x7FFF) {
+            return ram_enabled ? prg_ram[addr - 0x6000] : 0x00;
+        }
 
         uint16_t offset = addr & 0x3FFF;
         uint8_t mode = (control >> 2) & 0x03;
 
         switch (mode) {
-            case 0: case 1: // 32KB switching
+            case 0: case 1: // 32KB switching mode
             {
                 uint8_t bank = (prg_bank & 0x0E);
                 return prg_rom[(bank + (addr >= 0xC000)) % 4][offset];
             }
-            case 2: // Fix first bank, switch second
+            case 2: // Fix first bank ($8000), switch second ($C000)
                 if (addr < 0xC000) return prg_rom[0][offset];
                 return prg_rom[prg_bank % 4][offset];
-            case 3: // Switch first bank, fix last (DW Default)
+            case 3: // Switch first bank ($8000), fix last ($C000) - Standard for DW
                 if (addr < 0xC000) return prg_rom[prg_bank % 4][offset];
                 return prg_rom[3][offset];
         }
@@ -82,13 +93,17 @@ public:
             if (addr < 0x1000) return chr_rom[chr_bank_0 % 8][offset];
             return chr_rom[chr_bank_1 % 8][offset];
         } else {
-            return chr_rom[((chr_bank_0 & 0x1E) + (addr >= 0x1000)) % 8][offset];
+            // 8KB Mode uses chr_bank_0 and ignores bit 0
+            uint8_t bank = (chr_bank_0 & 0x1E);
+            if (addr >= 0x1000) bank++;
+            return chr_rom[bank % 8][offset];
         }
     }
-    
-    // Helper to handle writes to PRG RAM
+
     void write_prg_ram(uint16_t addr, uint8_t val) {
-        if (addr >= 0x6000 && addr <= 0x7FFF) prg_ram[addr - 0x6000] = val;
+        if (ram_enabled && addr >= 0x6000 && addr <= 0x7FFF) {
+            prg_ram[addr - 0x6000] = val;
+        }
     }
 };
 
