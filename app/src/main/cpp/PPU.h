19d94eca5f5ddc4e9a101d1ec4db7ea9ff70511d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-02-06 09:01:57 -0700

    Update PPU.h

--- app/src/main/cpp/PPU.h
+++ app/src/main/cpp/PPU.h
@@ -2,8 +2,8 @@
 #define PPU_H
 
 #include <cstdint>
-#include <vector>
 #include <mutex>
+#include "MapperMMC1.h"
 
 class PPU {
 public:
@@ -12,26 +12,21 @@ public:
     uint8_t oam_ram[256];
     uint32_t screen_buffer[256 * 240];
     
-    // Internal Registers
+    // Registers
     uint8_t ctrl;    // $2000
     uint8_t mask;    // $2001
     uint8_t status;  // $2002
     uint8_t oam_addr; // $2003
     
-    // Address latching logic ($2006)
     uint16_t addr_reg;
     uint8_t addr_latch;
     uint8_t data_buffer;
 
     PPU();
     void reset();
-    
-    // Bus Interface
-    uint8_t cpu_read(uint16_t addr);
-    void cpu_write(uint16_t addr, uint8_t val);
-    
-    // Rendering
-    void render_frame(class MapperMMC1* mapper);
+    uint8_t cpu_read(uint16_t addr, MapperMMC1& mapper);
+    void cpu_write(uint16_t addr, uint8_t val, MapperMMC1& mapper);
+    void render_frame(MapperMMC1& mapper, const uint32_t* nes_palette);
     
 private:
     uint16_t get_mirrored_addr(uint16_t addr);
