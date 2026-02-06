2a1b0b77ae653321d6ac8e8f4c894d07b104935d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-02-06 00:05:41 -0700

    Update MapperMMC1.h

--- app/src/main/cpp/MapperMMC1.h
+++ app/src/main/cpp/MapperMMC1.h
@@ -5,12 +5,14 @@
 
 class MapperMMC1 {
 public:
-    // Memory Storage: Public so native-lib can load ROM data into them
+    // Dragon Warrior (NES) specific memory layout:
+    // 64KB PRG ROM = 4 banks of 16KB
+    // 8KB CHR ROM = 2 banks of 4KB (Your header has 8, which is fine for compatibility)
     uint8_t prg_rom[4][16384]; 
     uint8_t chr_rom[8][4096];
     uint8_t prg_ram[8192]; 
 
-    // Mapper State
+    // Mapper State Registers
     uint8_t shift_register;
     uint8_t write_count; 
     uint8_t control;
@@ -19,7 +21,7 @@ public:
     uint8_t prg_bank;
     bool ram_enabled;
 
-    MapperMMC1(); // Constructor (declared here, defined in .cpp)
+    MapperMMC1();
 
     void reset();
     void write(uint16_t addr, uint8_t val);
