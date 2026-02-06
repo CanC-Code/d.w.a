58c5d35c3fdb262b6c9136e805b1759058d980b8
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-02-06 10:26:27 -0700

    Update MapperMMC1.h

--- app/src/main/cpp/MapperMMC1.h
+++ app/src/main/cpp/MapperMMC1.h
@@ -5,10 +5,9 @@
 
 class MapperMMC1 {
 public:
-    // Dragon Warrior I: 64KB PRG (4x16KB) and 16KB CHR
-    // Memory arrays used by the loader and the bus
+    // Dragon Warrior I: 64KB PRG (4x16KB) and 16KB CHR (4x4KB)
     uint8_t prg_rom[4][16384]; 
-    uint8_t chr_rom[8][4096];
+    uint8_t chr_rom[4][4096]; // Corrected to 4 banks for 16KB CHR
     uint8_t prg_ram[8192]; 
 
     // Mapper State Registers
@@ -18,17 +17,25 @@ public:
     uint8_t chr_bank_0;
     uint8_t chr_bank_1;
     uint8_t prg_bank;
-    bool ram_enabled; // Added to fix "use of undeclared identifier" error
+    bool ram_enabled;
 
-    // Constructor: Declaration only
+    // Constructor
     MapperMMC1();
 
-    // Hardware Interface: Declarations only
-    // Logic is moved to MapperMMC1.cpp to prevent redefinition errors
+    // Hardware Interface
     void reset();
+    
+    // CPU writes to $8000-$FFFF to control the mapper
     void write(uint16_t addr, uint8_t val);
+    
+    // CPU reads from $8000-$FFFF
     uint8_t read_prg(uint16_t addr);
+    
+    // PPU reads from $0000-$1FFF
     uint8_t read_chr(uint16_t addr);
+
+    // PPU writes to $0000-$1FFF (Required by PPU.cpp for stability)
+    void write_chr(uint16_t addr, uint8_t val);
 };
 
 #endif // MAPPERMMC1_H
