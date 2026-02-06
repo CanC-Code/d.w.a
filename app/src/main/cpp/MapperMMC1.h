03c52bc2ed45763938f8ae61d2c7358f4c2344cf
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-02-06 12:12:59 -0700

    Update MapperMMC1.h

--- app/src/main/cpp/MapperMMC1.h
+++ app/src/main/cpp/MapperMMC1.h
@@ -3,39 +3,57 @@
 
 #include <cstdint>
 
+/**
+ * MMC1 Mapper (iNES Mapper 1)
+ * Dragon Warrior I Specs: 
+ * PRG-ROM: 64KB (4 banks of 16KB)
+ * CHR-ROM: 8KB (2 banks of 4KB)
+ */
 class MapperMMC1 {
 public:
-    // Dragon Warrior I: 64KB PRG (4x16KB) and 16KB CHR (4x4KB)
+    // Memory Storage
     uint8_t prg_rom[4][16384]; 
-    uint8_t chr_rom[4][4096]; // Corrected to 4 banks for 16KB CHR
+    uint8_t chr_rom[2][4096]; // Standard DW1 has 8KB CHR
     uint8_t prg_ram[8192]; 
 
-    // Mapper State Registers
+    // Internal Shift Register
     uint8_t shift_register;
     uint8_t write_count; 
-    uint8_t control;
-    uint8_t chr_bank_0;
-    uint8_t chr_bank_1;
-    uint8_t prg_bank;
+
+    // Configuration Registers
+    uint8_t control;    // $8000-$9FFF
+    uint8_t chr_bank_0; // $A000-$BFFF
+    uint8_t chr_bank_1; // $C000-$DFFF
+    uint8_t prg_bank;   // $E000-$FFFF
     bool ram_enabled;
 
-    // Constructor
     MapperMMC1();
 
-    // Hardware Interface
+    // Reset to power-on state
     void reset();
-    
-    // CPU writes to $8000-$FFFF to control the mapper
+
+    // CPU interface for bank switching ($8000-$FFFF)
     void write(uint16_t addr, uint8_t val);
-    
-    // CPU reads from $8000-$FFFF
+
+    // CPU read access ($6000-$FFFF)
     uint8_t read_prg(uint16_t addr);
-    
-    // PPU reads from $0000-$1FFF
+
+    // PPU read access ($0000-$1FFF)
     uint8_t read_chr(uint16_t addr);
 
-    // PPU writes to $0000-$1FFF (Required by PPU.cpp for stability)
+    // PPU write access (typically ignored for CHR-ROM)
     void write_chr(uint16_t addr, uint8_t val);
+
+    /**
+     * Mirroring Mode Helper
+     * 0: One-Screen (Lower)
+     * 1: One-Screen (Upper)
+     * 2: Vertical (Dragon Warrior Default)
+     * 3: Horizontal
+     */
+    uint8_t get_mirroring_mode() const {
+        return control & 0x03;
+    }
 };
 
 #endif // MAPPERMMC1_H
