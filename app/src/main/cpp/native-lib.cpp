dbff0d6d4ff7e8b80576e6bcdf3e85dc092a96a4
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-02-06 18:55:30 -0700

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,269 +8,166 @@
 #include <chrono>
 #include <mutex>
 #include <cstring>
+
 #include "MapperMMC1.h"
+#include "PPU.h"
 #include "recompiled/cpu_shared.h"
 
 #define LOG_TAG "DWA_NATIVE"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-// --- Global Hardware State Definitions ---
-// These are declared as 'extern' in cpu_shared.h; they must be defined here.
+// --- Global Hardware State ---
 uint16_t reg_PC = 0;
 uint8_t reg_A = 0, reg_X = 0, reg_Y = 0, reg_S = 0xFD, reg_P = 0x24;
 uint8_t cpu_ram[0x0800] = {0};
 bool is_running = false;
 bool is_paused = false;
 
-// Internal Emulator State
-uint8_t ppu_vram[2048] = {0};
-uint8_t palette_ram[32] = {0};
-uint8_t oam_ram[256] = {0};
-uint32_t screen_buffer[256 * 240] = {0};
+// Controller State
 uint8_t controller_state = 0;
 uint8_t controller_shift = 0;
-uint8_t ppu_data_buffer = 0;
-uint8_t ppu_status = 0, ppu_ctrl = 0, ppu_mask = 0, ppu_addr_latch = 0, oam_addr = 0;
-uint16_t ppu_addr_reg = 0;
-std::mutex buffer_mutex;
+
+// Hardware Instances
 MapperMMC1 mapper;
+PPU ppu;
+std::mutex buffer_mutex;
 
 // Recompiled Dispatcher Namespace
 namespace Dispatcher { void execute(); }
 
-// 
-uint32_t nes_palette[64] = {
+// NES Color Palette (64 Colors)
+const uint32_t nes_palette[64] = {
     0xFF666666, 0xFF002A88, 0xFF1412A7, 0xFF3B00A4, 0xFF5C007E, 0xFF6E0040, 0xFF6C0600, 0xFF561D00,
     0xFF333500, 0xFF0B4800, 0xFF005200, 0xFF004F08, 0xFF00404D, 0xFF000000, 0xFF000000, 0xFF000000,
     0xFFADADAD, 0xFF155FD9, 0xFF4240FF, 0xFF7527FE, 0xFFA01ACC, 0xFFB71E7B, 0xFFB53120, 0xFF994E00,
     0xFF6B6D00, 0xFF388700, 0xFF0C9300, 0xFF008F32, 0xFF007C8D, 0xFF000000, 0xFF000000, 0xFF000000,
     0xFFFFFFFF, 0xFF64B0FF, 0xFF9290FF, 0xFFC676FF, 0xFFF36AFF, 0xFFFE6ECC, 0xFFFE8170, 0xFFEA9E22,
-    0xFFBCBE00, 0xFF88D100, 0xFF5CE430, 0xFF45E082, 0xFF48CDDE, 0xFF4F4F4F, 0xFF000000, 0xFF000000
+    0xFFBCBE00, 0xFF88D100, 0xFF5CE430, 0xFF45E082, 0xFF48CDDE, 0xFF4F4F4F, 0xFF000000, 0xFF000000,
+    0xFFFFFFFF, 0xFFABE7FF, 0xFFC7D1FF, 0xFFD9C1FF, 0xFFEBB7FF, 0xFFFFB7E9, 0xFFFFC0D0, 0xFFFFCBAB,
+    0xFFF1E094, 0xFFD7EB94, 0xFFB9F1B2, 0xFFAEF1D1, 0xFFAFF1F1, 0xFFC8C8C8, 0xFF000000, 0xFF000000
 };
 
 // --- Shared 6502 CPU Implementation ---
-// These functions match the 'extern "C"' declarations in cpu_shared.h
 extern "C" {
 
-    void execute_instruction() {
-        Dispatcher::execute();
-    }
+void execute_instruction() { Dispatcher::execute(); }
 
-    void update_nz(uint8_t val) {
-        reg_P &= ~(FLAG_Z | FLAG_N);
-        if (val == 0) reg_P |= FLAG_Z;
-        if (val & 0x80) reg_P |= FLAG_N;
-    }
-
-    void update_flags_cmp(uint8_t reg, uint8_t val) {
-        reg_P &= ~(FLAG_N | FLAG_Z | FLAG_C);
-        if (reg >= val) reg_P |= FLAG_C;
-        if (reg == val) reg_P |= FLAG_Z;
-        if ((uint8_t)(reg - val) & 0x80) reg_P |= FLAG_N;
-    }
-
-    void cpu_adc(uint8_t val) {
-        uint16_t carry = (reg_P & FLAG_C) ? 1 : 0;
-        uint16_t sum = reg_A + val + carry;
-        // Overflow flag logic: (A^result) & (M^result) & 0x80
-        if (~(reg_A ^ val) & (reg_A ^ (uint8_t)sum) & 0x80) reg_P |= FLAG_V; else reg_P &= ~FLAG_V;
-        if (sum > 0xFF) reg_P |= FLAG_C; else reg_P &= ~FLAG_C;
-        reg_A = (uint8_t)sum;
-        update_nz(reg_A);
-    }
-
-    void cpu_sbc(uint8_t val) { cpu_adc(~val); }
-
-    void cpu_bit(uint8_t val) {
-        reg_P &= ~(FLAG_Z | FLAG_V | FLAG_N);
-        if ((val & reg_A) == 0) reg_P |= FLAG_Z;
-        reg_P |= (val & 0xC0); // Bits 7 and 6 of value go to N and V
-    }
-
-    uint8_t cpu_asl(uint8_t val) {
-        (val & 0x80) ? reg_P |= FLAG_C : reg_P &= ~FLAG_C;
-        uint8_t res = val << 1;
-        update_nz(res);
-        return res;
-    }
-
-    uint8_t cpu_lsr(uint8_t val) {
-        (val & 0x01) ? reg_P |= FLAG_C : reg_P &= ~FLAG_C;
-        uint8_t res = val >> 1;
-        update_nz(res);
-        return res;
-    }
-
-    uint8_t cpu_rol(uint8_t val) {
-        uint8_t old_c = (reg_P & FLAG_C) ? 1 : 0;
-        (val & 0x80) ? reg_P |= FLAG_C : reg_P &= ~FLAG_C;
-        uint8_t res = (val << 1) | old_c;
-        update_nz(res);
-        return res;
-    }
-
-    uint8_t cpu_ror(uint8_t val) {
-        uint8_t old_c = (reg_P & FLAG_C) ? 0x80 : 0;
-        (val & 0x01) ? reg_P |= FLAG_C : reg_P &= ~FLAG_C;
-        uint8_t res = (val >> 1) | old_c;
-        update_nz(res);
-        return res;
-    }
-
-    uint16_t read_pointer(uint16_t addr) {
-        return bus_read(addr) | (bus_read(addr + 1) << 8);
-    }
-
-    uint16_t read_pointer_indexed_x(uint16_t zp_addr) {
-        uint8_t ptr = (uint8_t)(zp_addr + reg_X);
-        uint16_t lo = bus_read(ptr);
-        uint16_t hi = bus_read((uint8_t)(ptr + 1));
-        return lo | (hi << 8);
-    }
-
-    uint16_t read_pointer_indexed_y(uint16_t zp_addr) {
-        uint16_t lo = bus_read(zp_addr);
-        uint16_t hi = bus_read((uint8_t)(zp_addr + 1));
-        return (lo | (hi << 8)) + reg_Y;
-    }
-
-    void push_stack(uint8_t val) { 
-        cpu_ram[0x0100 | reg_S] = val; 
-        reg_S--; 
-    }
-    
-    uint8_t pop_stack() { 
-        reg_S++; 
-        return cpu_ram[0x0100 | reg_S]; 
-    }
-
-    // --- Bus Read Implementation ---
-    uint8_t bus_read(uint16_t addr) {
-        if (addr < 0x2000) return cpu_ram[addr % 0x0800];
-        if (addr >= 0x2000 && addr <= 0x3FFF) {
-            uint16_t reg = addr % 8;
-            if (reg == 2) { // PPUSTATUS
-                uint8_t s = ppu_status;
-                ppu_status &= ~0x80; // Clear V-Blank flag on read
-                ppu_addr_latch = 0;
-                return s;
-            }
-            if (reg == 7) { // PPUDATA
-                uint8_t data = ppu_data_buffer;
-                uint16_t p_addr = ppu_addr_reg & 0x3FFF;
-                if (p_addr < 0x2000) ppu_data_buffer = mapper.read_chr(p_addr);
-                else if (p_addr < 0x3F00) ppu_data_buffer = ppu_vram[ntable_mirror(p_addr)];
-                else data = read_palette(p_addr);
-                ppu_addr_reg += (ppu_ctrl & 0x04) ? 32 : 1;
-                return data;
-            }
-        }
-        if (addr >= 0x6000) return mapper.read_prg(addr);
-        if (addr == 0x4016) {
-            uint8_t ret = (controller_shift & 0x80) >> 7;
-            controller_shift <<= 1;
-            return ret;
-        }
-        return 0;
-    }
-
-    // --- Bus Write Implementation ---
-    void bus_write(uint16_t addr, uint8_t val) {
-        if (addr < 0x2000) cpu_ram[addr % 0x0800] = val;
-        else if (addr >= 0x2000 && addr <= 0x3FFF) {
-            uint16_t reg = addr % 8;
-            if (reg == 0) ppu_ctrl = val;
-            else if (reg == 1) ppu_mask = val;
-            else if (reg == 3) oam_addr = val;
-            else if (reg == 4) oam_ram[oam_addr++] = val;
-            else if (reg == 6) { // PPUADDR
-                if (ppu_addr_latch == 0) { ppu_addr_reg = (ppu_addr_reg & 0x00FF) | (val << 8); ppu_addr_latch = 1; }
-                else { ppu_addr_reg = (ppu_addr_reg & 0xFF00) | val; ppu_addr_latch = 0; }
-            }
-            else if (reg == 7) { // PPUDATA
-                uint16_t p_addr = ppu_addr_reg & 0x3FFF;
-                if (p_addr >= 0x2000 && p_addr < 0x3F00) ppu_vram[ntable_mirror(p_addr)] = val;
-                else if (p_addr >= 0x3F00) {
-                    uint8_t p_idx = p_addr & 0x1F;
-                    if (p_idx >= 0x10 && (p_idx % 4 == 0)) p_idx -= 0x10;
-                    palette_ram[p_idx] = val;
-                }
-                ppu_addr_reg += (ppu_ctrl & 0x04) ? 32 : 1;
-            }
-        }
-        else if (addr == 0x4014) { // OAMDMA
-            uint16_t base = val << 8;
-            for (int i = 0; i < 256; i++) oam_ram[i] = bus_read(base + i);
-        }
-        else if (addr == 0x4016) { 
-            if ((val & 0x01) == 0) controller_shift = controller_state; 
-        }
-        else if (addr >= 0x6000) mapper.write(addr, val);
-    }
-
-    void clear_screen_to_black() {
-        std::lock_guard<std::mutex> lock(buffer_mutex);
-        for (int i = 0; i < 256 * 240; i++) screen_buffer[i] = 0xFF000000;
-    }
-
-    uint16_t ntable_mirror(uint16_t addr) {
-        addr = (addr - 0x2000) % 0x1000;
-        return addr % 0x0800; // Simplified Horizontal/Vertical mirroring
-    }
-
-    uint8_t read_palette(uint16_t addr) {
-        uint8_t p_idx = addr & 0x1F;
-        if (p_idx >= 0x10 && (p_idx % 4 == 0)) p_idx -= 0x10;
-        return palette_ram[p_idx];
-    }
-} // End of extern "C"
-
-// --- Helper Functions ---
-
-void draw_frame() {
-    if ((ppu_mask & 0x18) == 0) return;
-    
-    std::lock_guard<std::mutex> lock(buffer_mutex);
-    uint16_t nt_base = (ppu_ctrl & 0x03) * 1024;
-    uint16_t bg_pat_base = (ppu_ctrl & 0x10) ? 0x1000 : 0x0000;
-    uint16_t attr_base = nt_base + 0x3C0;
-
-    for (int ty = 0; ty < 30; ty++) {
-        for (int tx = 0; tx < 32; tx++) {
-            uint8_t tile = ppu_vram[ntable_mirror(0x2000 + nt_base + ty * 32 + tx)];
-            uint16_t attr_addr = attr_base + (ty / 4) * 8 + (tx / 4);
-            uint8_t attr_byte = ppu_vram[ntable_mirror(0x2000 + attr_addr)];
-            uint8_t shift = ((ty & 2) << 1) | (tx & 2);
-            uint8_t palette_select = (attr_byte >> shift) & 0x03;
-            
-            for (int y = 0; y < 8; y++) {
-                uint8_t p1 = mapper.read_chr(bg_pat_base + tile * 16 + y);
-                uint8_t p2 = mapper.read_chr(bg_pat_base + tile * 16 + y + 8);
-                for (int x = 0; x < 8; x++) {
-                    uint8_t pix = ((p1 >> (7 - x)) & 0x01) | (((p2 >> (7 - x)) & 0x01) << 1);
-                    uint8_t palette_addr = (palette_select * 4) + pix;
-                    uint8_t palette_val = read_palette(0x3F00 + palette_addr);
-                    screen_buffer[(ty * 8 + y) * 256 + (tx * 8 + x)] = nes_palette[palette_val & 0x3F];
-                }
-            }
-        }
-    }
+void update_nz(uint8_t val) {
+    reg_P &= ~(FLAG_Z | FLAG_N);
+    if (val == 0) reg_P |= FLAG_Z;
+    if (val & 0x80) reg_P |= FLAG_N;
 }
 
-extern "C" void power_on_reset() {
+void update_flags_cmp(uint8_t reg, uint8_t val) {
+    reg_P &= ~(FLAG_N | FLAG_Z | FLAG_C);
+    if (reg >= val) reg_P |= FLAG_C;
+    if (reg == val) reg_P |= FLAG_Z;
+    if ((uint8_t)(reg - val) & 0x80) reg_P |= FLAG_N;
+}
+
+void cpu_adc(uint8_t val) {
+    uint16_t carry = (reg_P & FLAG_C) ? 1 : 0;
+    uint16_t sum = reg_A + val + carry;
+    if (~(reg_A ^ val) & (reg_A ^ (uint8_t)sum) & 0x80) reg_P |= FLAG_V; else reg_P &= ~FLAG_V;
+    if (sum > 0xFF) reg_P |= FLAG_C; else reg_P &= ~FLAG_C;
+    reg_A = (uint8_t)sum;
+    update_nz(reg_A);
+}
+
+void cpu_sbc(uint8_t val) { cpu_adc(~val); }
+
+void cpu_bit(uint8_t val) {
+    reg_P &= ~(FLAG_Z | FLAG_V | FLAG_N);
+    if ((val & reg_A) == 0) reg_P |= FLAG_Z;
+    reg_P |= (val & 0xC0);
+}
+
+uint8_t cpu_asl(uint8_t val) {
+    (val & 0x80) ? reg_P |= FLAG_C : reg_P &= ~FLAG_C;
+    uint8_t res = val << 1;
+    update_nz(res);
+    return res;
+}
+
+uint8_t cpu_lsr(uint8_t val) {
+    (val & 0x01) ? reg_P |= FLAG_C : reg_P &= ~FLAG_C;
+    uint8_t res = val >> 1;
+    update_nz(res);
+    return res;
+}
+
+uint8_t cpu_rol(uint8_t val) {
+    uint8_t old_c = (reg_P & FLAG_C) ? 1 : 0;
+    (val & 0x80) ? reg_P |= FLAG_C : reg_P &= ~FLAG_C;
+    uint8_t res = (val << 1) | old_c;
+    update_nz(res);
+    return res;
+}
+
+uint8_t cpu_ror(uint8_t val) {
+    uint8_t old_c = (reg_P & FLAG_C) ? 0x80 : 0;
+    (val & 0x01) ? reg_P |= FLAG_C : reg_P &= ~FLAG_C;
+    uint8_t res = (val >> 1) | old_c;
+    update_nz(res);
+    return res;
+}
+
+// --- Memory Bus ---
+
+uint8_t bus_read(uint16_t addr) {
+    if (addr < 0x2000) return cpu_ram[addr % 0x0800];
+    if (addr >= 0x2000 && addr <= 0x3FFF) return ppu.cpu_read(addr, mapper);
+    if (addr >= 0x6000) return mapper.read_prg(addr);
+    if (addr == 0x4016) {
+        uint8_t ret = (controller_shift & 0x80) >> 7;
+        controller_shift <<= 1;
+        return ret;
+    }
+    return 0;
+}
+
+void bus_write(uint16_t addr, uint8_t val) {
+    if (addr < 0x2000) cpu_ram[addr % 0x0800] = val;
+    else if (addr >= 0x2000 && addr <= 0x3FFF) ppu.cpu_write(addr, val, mapper);
+    else if (addr == 0x4014) { // OAM DMA
+        uint16_t base = val << 8;
+        for (int i = 0; i < 256; i++) ppu.oam_ram[i] = bus_read(base + i);
+    }
+    else if (addr == 0x4016) { 
+        if ((val & 0x01) == 0) controller_shift = controller_state; 
+    }
+    else if (addr >= 0x6000) mapper.write(addr, val);
+}
+
+// --- Helpers Required by cpu_shared.h ---
+
+uint16_t read_pointer(uint16_t addr) { return bus_read(addr) | (bus_read(addr + 1) << 8); }
+uint16_t read_pointer_indexed_x(uint16_t zp) { return bus_read((uint8_t)(zp + reg_X)) | (bus_read((uint8_t)(zp + reg_X + 1)) << 8); }
+uint16_t read_pointer_indexed_y(uint16_t zp) { return (bus_read(zp) | (bus_read((uint8_t)(zp + 1)) << 8)) + reg_Y; }
+void push_stack(uint8_t val) { cpu_ram[0x0100 + (reg_S--)] = val; }
+uint8_t pop_stack() { return cpu_ram[0x0100 + (++reg_S)]; }
+void clear_screen_to_black() { std::lock_guard<std::mutex> lock(buffer_mutex); std::memset(ppu.screen_buffer, 0, 256 * 240 * 4); }
+
+} // extern "C"
+
+// --- Emulator Logic ---
+
+void power_on_reset() {
     mapper.reset();
-    uint8_t lo = mapper.prg_rom[3][0x3FFC];
-    uint8_t hi = mapper.prg_rom[3][0x3FFD];
+    ppu.reset();
+    uint8_t lo = bus_read(0xFFFC);
+    uint8_t hi = bus_read(0xFFFD);
     reg_PC = (hi << 8) | lo;
-    if (reg_PC < 0x8000) reg_PC = 0xC000;
     reg_S = 0xFD; reg_P = 0x24; reg_A = 0; reg_X = 0; reg_Y = 0;
 }
 
-extern "C" void nmi_handler() {
+void trigger_nmi() {
     push_stack(reg_PC >> 8);
     push_stack(reg_PC & 0xFF);
     push_stack(reg_P);
-    reg_PC = (bus_read(0xFFFB) << 8) | bus_read(0xFFFA);
-    ppu_status |= 0x80;
+    reg_PC = bus_read(0xFFFA) | (bus_read(0xFFFB) << 8);
+    ppu.status |= 0x80;
 }
 
 void engine_loop() {
@@ -278,18 +175,27 @@ void engine_loop() {
     is_running = true;
     while (is_running) {
         if (is_paused) { std::this_thread::sleep_for(std::chrono::milliseconds(16)); continue; }
+        
         auto start = std::chrono::steady_clock::now();
-        for (int i = 0; i < 29780; i++) { // ~1 frame of cycles
+        // Execute ~1 frame worth of cycles (approx 29780)
+        for (int i = 0; i < 29780; i++) {
             execute_instruction();
-            if (!is_running) break;
         }
-        nmi_handler();
-        draw_frame();
+
+        // Handle VBlank and NMI
+        trigger_nmi();
+        
+        // Render to internal buffer
+        {
+            std::lock_guard<std::mutex> lock(buffer_mutex);
+            ppu.render_frame(mapper, nes_palette);
+        }
+
         std::this_thread::sleep_until(start + std::chrono::microseconds(16666));
     }
 }
 
-// --- JNI Methods ---
+// --- JNI Interface (Fixed Method Names) ---
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_canc_dwa_MainActivity_nativeExtractRom(JNIEnv *env, jobject thiz, jstring romPath, jstring outDir) {
@@ -299,8 +205,7 @@ Java_com_canc_dwa_MainActivity_nativeExtractRom(JNIEnv *env, jobject thiz, jstri
 
     uint8_t header[16];
     file.read((char*)header, 16);
-    if (header[0] != 'N' || header[1] != 'E' || header[2] != 'S') { file.close(); env->ReleaseStringUTFChars(romPath, path); return env->NewStringUTF("Error: Invalid NES"); }
-
+    // Dragon Warrior 1 iNES: 4x16KB PRG, 2x8KB CHR (standard for MMC1 variant)
     for (int i = 0; i < 4; i++) file.read((char*)mapper.prg_rom[i], 16384);
     for (int i = 0; i < 2; i++) file.read((char*)mapper.chr_rom[i], 4096);
     
@@ -311,7 +216,9 @@ Java_com_canc_dwa_MainActivity_nativeExtractRom(JNIEnv *env, jobject thiz, jstri
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_canc_dwa_MainActivity_nativeInitEngine(JNIEnv *env, jobject thiz, jstring filesDir) {
-    if (!is_running) { clear_screen_to_black(); std::thread(engine_loop).detach(); }
+    if (!is_running) {
+        std::thread(engine_loop).detach();
+    }
 }
 
 extern "C" JNIEXPORT void JNICALL
@@ -320,12 +227,17 @@ Java_com_canc_dwa_MainActivity_nativeUpdateSurface(JNIEnv *env, jobject thiz, jo
     void* pixels;
     if (AndroidBitmap_getInfo(env, bitmap, &info) < 0) return;
     if (AndroidBitmap_lockPixels(env, bitmap, &pixels) < 0) return;
-    { std::lock_guard<std::mutex> lock(buffer_mutex); memcpy(pixels, screen_buffer, 256 * 240 * 4); }
+    
+    {
+        std::lock_guard<std::mutex> lock(buffer_mutex);
+        std::memcpy(pixels, ppu.screen_buffer, 256 * 240 * 4);
+    }
+    
     AndroidBitmap_unlockPixels(env, bitmap);
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_canc_dwa_MainActivity_injectInput(JNIEnv *env, jobject thiz, jint buttonBit, jboolean isPressed) {
+Java_com_canc_dwa_MainActivity_nativeInjectInput(JNIEnv *env, jobject thiz, jint buttonBit, jboolean isPressed) {
     if (isPressed) controller_state |= (uint8_t)buttonBit;
     else controller_state &= ~((uint8_t)buttonBit);
 }
@@ -335,8 +247,3 @@ Java_com_canc_dwa_MainActivity_nativePauseEngine(JNIEnv *env, jobject thiz) { is
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_canc_dwa_MainActivity_nativeResumeEngine(JNIEnv *env, jobject thiz) { is_paused = false; }
-
-extern "C" JNIEXPORT void JNICALL
-Java_com_canc_dwa_MainActivity_nativeTriggerNMI(JNIEnv *env, jobject thiz) {
-    nmi_handler();
-}
